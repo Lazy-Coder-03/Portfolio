890c5284f6e3153b2056159d95122e8ef8d9f494
Lazy_coder <ghosh2003sayantan@gmail.com>
2023-10-21 02:21:48 +0530

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,71 @@
+let particles = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < 1200; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function draw() {
+    background(0); // Set the background color (black)
+
+    let attractor = createVector(mouseX, mouseY);
+    for (let particle of particles) {
+        let force = p5.Vector.sub(attractor, particle.pos);
+        let distance = force.mag();
+        // Adjusted minimum and maximum distance
+        distance = constrain(distance, 5, 200); // Increase the maximum distance
+
+        let strength = 0.05; // (1 / (distance * distance) )*10; // Increased attraction strength
+
+        force.setMag(strength);
+        particle.applyForce(force);
+        particle.update();
+        particle.display();
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(); // p5.Vector.random2D().mult(random(2, 5)); // Random initial velocities
+        this.acc = createVector();
+        this.size = random(2, 10);
+        this.r = random(255);
+        this.g = random(255);
+        this.b = random(255);
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.vel.limit(5); // Limit the particle's speed
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+        // if (this.pos.x < 0 || this.pos.x > width) {
+        //     this.vel.x *= -1;
+        // }
+        // if (this.pos.y < 0 || this.pos.y > height) {
+        //     this.vel.y *= -1;
+        // }
+
+        if(this.pos.x < 0) this.pos.x = width;
+        if(this.pos.x > width) this.pos.x = 0;
+        if(this.pos.y < 0) this.pos.y = height;
+        if(this.pos.y > height) this.pos.y = 0;
+    }
+
+    display() {
+        noStroke();
+        fill(this.r, this.g, this.b); // Particle color (white)
+        ellipse(this.pos.x, this.pos.y, this.size);
+    }
+}
---
+++ script.js
@@ -0,0 +1,122 @@
+// Fetch and display GitHub projects
+const githubProjects = document.getElementById("github-projects");
+
+// Fetch project data from the JSON file (projects.json)
+fetch("projects.json")
+    .then((response) => response.json())
+    .then((projects) => {
+        projects.forEach((project) => {
+            const projectCard = createProjectCard(project);
+            githubProjects.appendChild(projectCard);
+        });
+    })
+    .catch((error) => {
+        console.error("Error fetching project data:", error);
+    });
+
+function createProjectCard(project) {
+    const card = document.createElement("div");
+    card.classList.add("project-card");
+
+    const title = document.createElement("h3");
+    title.textContent = project.name;
+
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.classList.add("project-description");
+    descriptionDiv.textContent = project.description;
+    descriptionDiv.style.display = "none"; // Initially hidden
+
+    // Add event listeners to toggle the description visibility on hover
+    card.addEventListener("mouseenter", () => {
+        descriptionDiv.style.display = "block";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        descriptionDiv.style.display = "none";
+    });
+
+    const githubLink = createLink("GitHub Repo", project.githubLink);
+    const visitLink = createLink("Visit Project", project.visitLink);
+
+    const image = createImage(project.imageURL, project.name);
+
+    card.appendChild(title);
+    card.appendChild(descriptionDiv);
+    card.appendChild(githubLink);
+    card.appendChild(visitLink);
+    card.appendChild(image);
+
+    return card;
+}
+
+function createLink(text, link) {
+    const anchor = document.createElement("a");
+    anchor.textContent = text;
+    anchor.href = link;
+    anchor.target = "_blank";
+    return anchor;
+}
+
+function createImage(imageURL, altText) {
+    const image = document.createElement("img");
+    image.src = imageURL;
+    image.alt = altText;
+    return image;
+}
+
+// Fetch and display Blender animations
+const blenderAnimations = document.getElementById("blender-animations");
+
+// Fetch video data from the JSON file (blender.json)
+fetch("blender.json")
+    .then((response) => response.json())
+    .then((videos) => {
+        videos.forEach((video) => {
+            const videoElement = createVideoElement(video);
+            blenderAnimations.appendChild(videoElement);
+
+            // Add event listeners to control video playback on hover
+            videoElement.addEventListener("mouseenter", () => {
+                const videoPlayer = videoElement.querySelector("video");
+                videoPlayer.play();
+                videoPlayer.loop = true; // Enable looping
+            });
+
+            videoElement.addEventListener("mouseleave", () => {
+                const videoPlayer = videoElement.querySelector("video");
+                videoPlayer.pause();
+                videoPlayer.currentTime = 0; // Reset to the beginning
+            });
+        });
+    })
+    .catch((error) => {
+        console.error("Error fetching video data:", error);
+    });
+
+function createVideoElement(video) {
+    const videoContainer = document.createElement("div");
+    videoContainer.classList.add("video-container");
+
+    const videoTitle = document.createElement("h3");
+    videoTitle.textContent = video.name;
+
+    const videoPlayer = document.createElement("video");
+    videoPlayer.controls = true;
+    videoPlayer.width = 480;
+    videoPlayer.height = 270;
+
+    const videoSource = document.createElement("source");
+    videoSource.src = video.videoSource;
+    videoSource.type = "video/mp4";
+
+    const fallbackText = document.createElement("p");
+    fallbackText.textContent = "Your browser does not support the video tag.";
+
+    videoPlayer.appendChild(videoSource);
+    videoPlayer.appendChild(fallbackText);
+
+    videoContainer.appendChild(videoTitle);
+    videoContainer.appendChild(videoPlayer);
+
+    return videoContainer;
+}
